60ae25e1d2f1352f3d90919a9f4a90f50e3792aa
Bhavesh Kumar <bkm009@users.noreply.github.com>
2020-08-19 12:44:12 +0530

    Nil

--- cash_book_pc/settings.py
+++ cash_book_pc/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u$x)s7^dosvpdh7=s6ky^%re1uvcd7^k3l0!d=0p_vki0#!x+s'
+SECRET_KEY = 'SECRET'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
